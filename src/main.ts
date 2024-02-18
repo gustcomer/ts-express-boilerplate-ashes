884ec95dcaed4a06c595f5e84a6e6838531adc25
gustcomer <gustcomer@gmail.com>
2024-02-18 08:00:36 -0300

    feat: add GET /tickets/:ticketId endpoint

--- src/main.ts
+++ src/main.ts
@@ -15,4 +15,21 @@ app.post("/tickets", async function (req, res) {
 	});
 });
 
+app.get("/tickets/:ticketId", async function (req, res) {
+	const connection = pgp()("postgres://postgres:123456@localhost:5432/example");
+	const [ticketData] = await connection.query("select * from example.ticket where ticket_id = $1", [req.params.ticketId]);
+	const ticket = {
+		ticketId: ticketData.ticket_id,
+		requesterId: ticketData.requester_id,
+		assigneeId: ticketData.assignee_id,
+		startDate: ticketData.start_date,
+		endDate: ticketData.end_date,
+		content: ticketData.content,
+		status: ticketData.status,
+		duration: ticketData.duration
+	}
+	await connection.$pool.end();
+	res.json(ticket);
+});
+
 app.listen(3000);
