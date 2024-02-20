f775209bf733ae9a8d32a0922581c3452e6af00a
gustcomer <gustcomer@gmail.com>
2024-02-20 08:55:23 -0300

    feat: add route to update the tickets with assignee

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,15 @@ app.post("/tickets", async function (req, res) {
 	});
 });
 
+app.post("/tickets/:ticketId/assign", async function(req, res) {
+	const connection = pgp()("postgres://postgres:123456@localhost:5432/example");
+	const [ticketData] = await connection.query("select * from example.ticket where ticket_id = $1", [req.params.ticketId]);
+	if (ticketData.status === "closed") throw new Error("The ticket is closed");
+	await connection.query("update example.ticket set status = $1, assignee_id = $2 where ticket_id = $3", [req.body.status, req.body.assigneeId, req.params.ticketId]);
+	await connection.$pool.end();
+	res.end();
+});
+
 app.get("/tickets/:ticketId", async function (req, res) {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/example");
 	const [ticketData] = await connection.query("select * from example.ticket where ticket_id = $1", [req.params.ticketId]);
