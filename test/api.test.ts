b44e086a23407a4d6571b43f1c16ca1d198127a4
gustcomer <gustcomer@gmail.com>
2024-02-20 08:56:00 -0300

    test: Must assign the ticket to an attendee

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 test("Must create a new ticket", async function() {
-  // given, arrange
+  // given, arrange - that we have created a ticket
   const input1 = {
     requesterId: "b7e6db47-3e5d-4220-9265-0e6eef8c4330",
     content: "The internet is not working"
@@ -16,4 +16,24 @@ test("Must create a new ticket", async function() {
 	expect(output2.requesterId).toBe(input1.requesterId);
 	expect(output2.content).toBe(input1.content);
 	expect(output2.status).toBe("open");
+})
+
+test("Must assign the ticket to an attendee", async function() {
+	const input1 = {
+		requesterId: "b7e6db47-3e5d-4220-9265-0e6eef8c4330",
+		content: "A internet está muito lenta"
+	};
+  const response1 = await axios.post("http://localhost:3000/tickets", input1);
+  const output1 = response1.data;
+
+	const input2 = {
+		assigneeId: "d88aa636-22f5-4da1-9384-077f80e51301",
+		status: "assigned"
+	};
+  await axios.post(`http://localhost:3000/tickets/${output1.ticketId}/assign`, input2);
+
+	const response3 = await axios.get(`http://localhost:3000/tickets/${output1.ticketId}`);
+	const output3 = response3.data;
+	expect(output3.assigneeId).toBe(input2.assigneeId);
+	expect(output3.status).toBe("assigned");
 })
